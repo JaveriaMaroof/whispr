957e952b7158e4857b3479f6dadb447e2de0d61a
Aiman Maroof <aiman.maroofofficial@gmail.com>
2026-03-10 12:23:32 -0700

    fix backend urls

--- client/src/pages/SendMessage.jsx
+++ client/src/pages/SendMessage.jsx
@@ -13,7 +13,7 @@ export default function SendMessage(){
 
         try{
 
-            await axios.post("https://whispr-production-9678.up.railway.app/api/send-message",{
+            await axios.post(`${import.meta.env.VITE_API_URL}/api/send-message`,{
                 receiver: username,
                 text: text
             });
