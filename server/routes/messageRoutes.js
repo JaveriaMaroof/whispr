8135d99b2d647c0f319b4f943d7394f236058b7b
Aiman Maroof <aiman.maroofofficial@gmail.com>
2026-03-12 02:01:51 -0700

    fix user import

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const router = express.Router();
 const Message = require("../models/Message");
+const User = require("../models/User");
 
 router.post("/send-message", async (req,res)=>{
 
